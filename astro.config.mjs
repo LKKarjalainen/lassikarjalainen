0428ef66898dcbb0e04bf5e4a0c27a3e55beb96f
LKKarjalainen <lassikarjalainen0@gmail.com>
2025-11-01 23:21:20 +0200

    kontsaa taas jee

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://lassikarjalainen.fi',
   outDir: 'dist',
+  output: 'static',
 })
